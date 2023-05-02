96c00fd8886f20ed45926850c46df965c31dda97
Poolar78 <Hakim.Salaues@fundacion-jala.org>
2023-05-01 21:48:17 -0400

    large_cactus
    agregue large cactus a la tarea

--- dino_runner/components/obstacles/obstacleManager.py
+++ dino_runner/components/obstacles/obstacleManager.py
@@ -1,7 +1,8 @@
 from dino_runner.components.obstacles.cactus import Cactus
-from dino_runner.utils.constants import SMALL_CACTUS
+from dino_runner.components.obstacles.large_cactus import Large_cactus
+from dino_runner.utils.constants import SMALL_CACTUS, LARGE_CACTUS
 import pygame
-
+import random 
 
 class ObstacleManager:
     
@@ -12,12 +13,16 @@ class ObstacleManager:
     
     def update(self, game):
         if len(self.obstacles) == 0:
-            self.obstacles.append(Cactus(SMALL_CACTUS))
+            cactus_type = random.choice([SMALL_CACTUS, LARGE_CACTUS])
+            if cactus_type == SMALL_CACTUS:
+                self.obstacles.append(Cactus(cactus_type))
+            else:
+                self.obstacles.append(Large_cactus(cactus_type))
 
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed , self.obstacles)
             if game.player.rect.colliderect(obstacle.rect):
-                print("boom")
+                print("BOOOOOOOOM")
                 pygame.time.delay(500)
                 game.playing = False
 
